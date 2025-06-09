ab83555f5acf152c4594cc9812767df14a75ad57
anshumansharma25 <anshuman25sharma@gmail.com>
2025-06-10 00:31:21 +0530

    Admin product endpoint done

--- app/core/database.py
+++ app/core/database.py
@@ -5,7 +5,7 @@ from app.core.config import settings
 # Get DB URL from config
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
-# Create DB engine (SQLite or PostgreSQL). SQLite needs connect_args.
+# Create DB engine (SQLite). SQLite needs connect_args.
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
@@ -16,3 +16,11 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base class for all our ORM models
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
