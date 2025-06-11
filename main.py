878fc929f000078dca2c3dfeaa2cb5fd6bc3cce3
anshumansharma25 <anshuman25sharma@gmail.com>
2025-06-12 01:44:51 +0530

    cart and order functionality done

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from app.auth.routes import router as auth_router
 from app.admin.routes import router as admin_router
 from app.products.routes import router as product_router
 from app.cart.routes import router as cart_router
+from app.orders.routes import router as order_router
 
 # Create DB tables
 Base.metadata.create_all(bind=engine)
@@ -19,3 +20,4 @@ app.include_router(auth_router)
 app.include_router(admin_router)
 app.include_router(product_router)
 app.include_router(cart_router)
+app.include_router(order_router)
